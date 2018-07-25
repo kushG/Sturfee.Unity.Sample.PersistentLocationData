3f0a95bf3cde070f3626517bf88c8e8f2ae2d272
Newman Willis <newmanw@sturfee.com>
2018-07-24 18:28:45 -0700

    Moved all alignment code to custom script. Changed gaze targets to appear in one direction, one at a time. Added seeking arrow that points directly to targets.

---
+++ Assets/PersistentLocationData/Scripts/AlignmentManager.cs
@@ -0,0 +1,337 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Sturfee.Unity.XR.Core.Events;
+using Sturfee.Unity.XR.Core.Session;
+//using Sturfee.Unity.XR.Core.Constants;
+//using Sturfee.Unity.XR.Package.Utilities;
+
+
+// Customized Alignment script - adjusts the alignments circles or 'Gaze Targets' to be in one direction from the starting point, instead of to the left and right
+// 	- Also adds an arrow that points directly towards the circle regardless of camera position
+public class AlignmentManager : MonoBehaviour {
+
+	public static AlignmentManager Instance;
+
+	// TODO: finish incorporating 'CheckCoverageOnStart'
+	// TODO: Possibly change these public variables to private
+
+	[Header("Buttons")]
+	public GameObject ScanButton;
+	public GameObject BackButton;
+
+	[Header("Multiframe UI")]
+	public GameObject GazeTargetPrefab;  // TODO: Change name to 'Prefab'
+	public GameObject Cursor;
+	public GameObject Arrow;
+
+	[Header("Coverage")]
+	public bool CheckCoverageOnStart; // TODO: probably remove this bool and just check
+
+	private List<GameObject> _gazeTargets;
+	private int _totalTargets = 3;  // TODO: Change name - user only sees 2 targets....
+	private int _targetCount;
+	private bool _isScanning; 
+	private int _multiframeRequestId;
+	private bool _resetCalled;
+
+//	[Header("UI")]
+	[HideInInspector]
+	public bool SessionReady;
+
+	private int _angle = 50;
+//	private int _targetCount = 3;
+
+	private void Awake()
+	{            
+		Instance = this;
+
+		SturfeeEventManager.Instance.OnSessionReady += OnSessionReady;
+		SturfeeEventManager.Instance.OnSessionFailed += OnSessionFailed;
+		SturfeeEventManager.Instance.OnCoverageCheckComplete += OnCoverageCheckComplete;
+		SturfeeEventManager.Instance.OnLocalizationComplete += OnLocalizationComplete;
+		SturfeeEventManager.Instance.OnLocalizationLoading += OnLocalizationLoading;
+
+		Cursor.SetActive (false);
+		Arrow.SetActive (false);
+	}
+
+	private void Start () {
+		_targetCount = _totalTargets - 1;
+
+	}
+
+	private void OnDestroy()
+	{
+		// *IMPORTANT* Unregister event handlers for Sturfee events
+
+		// XR SESSION
+		SturfeeEventManager.Instance.OnSessionReady -= OnSessionReady;
+		SturfeeEventManager.Instance.OnSessionFailed -= OnSessionFailed;
+
+		// LOCALIZATION
+		SturfeeEventManager.Instance.OnCoverageCheckComplete -= OnCoverageCheckComplete;
+		SturfeeEventManager.Instance.OnLocalizationComplete -= OnLocalizationComplete;
+		SturfeeEventManager.Instance.OnLocalizationLoading -= OnLocalizationLoading;
+
+	}
+
+	public void SetScanButton(string scanButtonText)
+	{
+		ScanButton.GetComponentInChildren<Text> ().text = scanButtonText;
+
+		if (SessionReady)
+		{
+			BackButton.SetActive (true);
+			ScanButton.SetActive (true);
+		}
+		else
+		{
+			ScreenMessageController.Instance.SetText ("Initializing Session...");
+		}
+	}
+
+	public void OnScanClick()
+	{
+		ScreenMessageController.Instance.SetText ("Aligning Camera...");
+		BackButton.SetActive (false);
+
+//		MultiframeManager.Instance.OnScanButtonClick ();
+
+		_isScanning = true;
+
+		print ("*** MULTIFRAME CALL START ***");
+
+		StartCoroutine(MultiframeCallAsync());
+
+		//		AlignmentManager.Instance.Capture ();
+
+		//		AlignmentInstrText.SetActive (true);
+		//		XRSessionManager.GetSession ().PerformLocalization ();
+	}
+
+	public List<GameObject> GetGazeTargets()
+	{
+		return _gazeTargets;
+	}
+
+	private IEnumerator MultiframeCallAsync()
+	{
+		print ("*** MULTIFRAME CALL ACTIVE ***");
+
+		//Setup variables for starting multiframe requests
+		SetupMultiframe();
+
+		//Create gaze targets
+		CreateGazeTargets();
+
+		//wait till all the requests are complete
+		while (_multiframeRequestId != _totalTargets)
+		{
+			yield return null;
+		}
+
+		print ("*** MULTIFRAME CALL COMPLETE ***");
+
+		Cursor.SetActive(false);
+
+
+		_isScanning = false;
+
+		Arrow.SetActive (false);
+
+	}
+
+	private void SetupMultiframe()
+	{
+		_resetCalled = false;
+
+		_gazeTargets = new List<GameObject>();
+
+		Cursor.SetActive(true);
+
+		_multiframeRequestId = 0;
+
+		AddToMultiframeLocalizationCall();
+	}
+
+	private void CreateGazeTargets()
+	{
+		for (int i = 1; i <= _targetCount; i++)
+		{
+			int j;
+			Vector3 dir;
+
+
+			var gaze = Instantiate (GazeTargetPrefab);
+			dir = Quaternion.AngleAxis (_angle * i, Vector3.up) * (XRSessionManager.GetSession ().GetXRCameraOrientation () * Vector3.forward);
+			gaze.transform.position = XRSessionManager.GetSession ().GetXRCameraPosition () + (dir * 10.0f);
+			gaze.transform.LookAt (GameObject.FindGameObjectWithTag ("XRCamera").transform);
+
+			gaze.name = "Gaze target (" + (_angle * i) + ")";
+
+			_gazeTargets.Add (gaze);
+
+			gaze.SetActive (false);
+		}
+
+		Arrow.SetActive (true);
+		StartCoroutine(CheckGazeTargetHit(_gazeTargets[0]));
+	}
+
+
+
+	private IEnumerator CheckGazeTargetHit(GameObject target)
+	{
+		_gazeTargets [_multiframeRequestId - 1].SetActive (true);
+		Arrow.GetComponent<MultiframeArrow>().SetTarget (target.transform);
+
+		RaycastHit hit;
+		bool done = false;
+
+		while (!done && !_resetCalled)
+		{
+			Camera xrCamera = GameObject.FindGameObjectWithTag("XRCamera").GetComponent<Camera>();
+			//Debug.DrawRay(xrCamera.transform.position, xrCamera.transform.forward * 1000, Color.green, 2000);
+
+			//TODO: Exclude building/terrain layers
+			if (Physics.Raycast(xrCamera.transform.position, xrCamera.transform.forward, out hit, Mathf.Infinity))//, ~LayerMask.NameToLayer("Multiframe UI")))
+			{
+				if (hit.transform.gameObject == target)
+				{                    
+					done = true;
+				}
+				else
+				{
+					yield return null;
+				}
+			}
+			else
+			{
+				yield return null;
+			}
+		}
+
+		Destroy(target);
+
+		AddToMultiframeLocalizationCall();
+
+	}
+
+	private void AddToMultiframeLocalizationCall()
+	{
+		_multiframeRequestId++;
+
+		//If reset is called, send -1 otherwise the count
+		XRSessionManager.GetSession().PerformLocalization((_resetCalled) ? -1 : _totalTargets /*TargetCount*/);
+
+		if (_multiframeRequestId > 1 && _multiframeRequestId < _totalTargets)
+		{
+			// Activate the next gaze target
+			StartCoroutine( CheckGazeTargetHit(_gazeTargets[_multiframeRequestId - 1]) );
+		}
+
+	}
+
+	#region XR SESSION
+	private void OnSessionReady()
+	{
+		ScreenMessageController.Instance.ClearText ();
+
+		SessionReady = true;
+
+
+
+
+		if (!GameManager.Instance.HasSaveDataPanel.activeSelf)
+		{
+			ScanButton.SetActive (true);
+
+			if (GameManager.Instance.HasSaveData)
+			{
+				BackButton.SetActive (true);
+			}
+		}
+
+
+//		_sessionIsReady = true;
+//
+//		if (!CheckCoverageOnStart)
+//		{
+//			ScanButton.SetActive(true);
+//		}
+//		else
+//		{
+//			XRSessionManager.GetSession().CheckCoverage();
+//		}
+	}
+
+	private void OnSessionFailed (string error)
+	{
+		ScreenMessageController.Instance.SetText("Session Intialization failed : " + error);
+//		ToastManager.Instance.ShowToast("Session Intialization failed : " + error);
+	}
+
+	private void OnCoverageCheckComplete (bool result)
+	{
+		if(result == false)
+		{
+			ScreenMessageController.Instance.SetText("Localization not available at this location. Localization calls won't work", 5);
+			return;
+		}
+
+		Debug.Log("Localization available");
+
+		if(CheckCoverageOnStart)
+		{
+			ScanButton.SetActive(true);
+		}
+	}
+	#endregion
+
+	#region LOCALIZATION
+	public void Capture()
+	{            
+//		_scanManager.OnScanButtonClick();
+	}
+
+	private void OnLocalizationLoading()
+	{
+//		_scanManager.PlayScanAnimation();
+	}
+
+	private void OnLocalizationComplete (Sturfee.Unity.XR.Core.Constants.Enums.AlignmentStatus status)
+	{
+		print ("*** ALIGNMENT MANAGER LOCALIZATION COMPLETE ***");
+
+		if (status == Sturfee.Unity.XR.Core.Constants.Enums.AlignmentStatus.Done)
+		{
+			ScreenMessageController.Instance.SetText ("Camera Alignment Complete", 3);
+
+			GameManager.Instance.InitializeGame2 ();
+
+//			Debug.Log ("Localization Complete");
+//			ToastManager.Instance.ShowToastTimed("Tap anywhere to place planes");
+		}
+		else if (status == Sturfee.Unity.XR.Core.Constants.Enums.AlignmentStatus.OutOfCoverage)
+		{
+			ScreenMessageController.Instance.SetText("Localization not available at this location");
+			GameManager.Instance.ResetToStartScreen ();
+		}
+		else if (status == Sturfee.Unity.XR.Core.Constants.Enums.AlignmentStatus.IndoorsError)
+		{
+			ScreenMessageController.Instance.SetText("Localization Failed: Indoors Error");
+			GameManager.Instance.ResetToStartScreen ();
+		}
+		else if (status == Sturfee.Unity.XR.Core.Constants.Enums.AlignmentStatus.Error)
+		{
+			ScreenMessageController.Instance.SetText("Localization failed");
+			GameManager.Instance.ResetToStartScreen ();
+		}
+
+
+	}
+	#endregion
+
+}
--- Assets/PersistentLocationData/Scripts/GameManager.cs
+++ Assets/PersistentLocationData/Scripts/GameManager.cs
@@ -24,6 +24,11 @@ public class GameManager : MonoBehaviour {
 	public GameObject Tier2ItemPrefab;
 	public GameObject Tier3ItemPrefab;
 
+//	[Header("Other")]
+	[HideInInspector]
+	public bool HasSaveData;
+	public static bool LoadGame;
+
 	private bool _hasSaveData;
 	private bool _loadGame = false;
 	private bool _sessionReady = false;
@@ -32,13 +37,13 @@ public class GameManager : MonoBehaviour {
 	{
 		Instance = this;
 
-		SturfeeEventManager.Instance.OnSessionReady += OnSessionReady;
-		SturfeeEventManager.Instance.OnLocalizationComplete += OnLocalizationComplete;
+//		SturfeeEventManager.Instance.OnSessionReady += OnSessionReady;
+//		SturfeeEventManager.Instance.OnLocalizationComplete += OnLocalizationComplete;
 
 		HasSaveDataPanel.SetActive (false);
-		ScanButton.SetActive (false);
+//		ScanButton.SetActive (false);
 		BackButton.SetActive (false);
-		AlignmentInstrText.SetActive (false);
+//		AlignmentInstrText.SetActive (false);
 	}
 
 	void Start ()
@@ -54,11 +59,15 @@ public class GameManager : MonoBehaviour {
 		}
 	}
 
+	// TODO: Change this name
 	public void OnStartScreenButtonClick(bool loadGame)
 	{
 
 		_loadGame = loadGame;
+		LoadGame = loadGame;
 		HasSaveDataPanel.SetActive (false);
+
+		// TODO: Move this code below to AlignmentManager
 
 		string scanButtonText;// = ScanButton.GetComponentInChildren<Text>().text;
 		if (loadGame)
@@ -72,81 +81,82 @@ public class GameManager : MonoBehaviour {
 			scanButtonText = "Scan (New Game)";
 //			ScanButton.GetComponentInChildren<Text>().text = "Scan (New Game)";
 		}
-//		AlignmentManager.Instance.
-		ScanButton.GetComponentInChildren<Text> ().text = scanButtonText;
 
-		if (_sessionReady)
-		{
-			BackButton.SetActive (true);
-//			AlignmentManager.Instance.
-			ScanButton.SetActive (true);
-		}
-		else
-		{
-			ScreenMessageController.Instance.SetText ("Initializing Session...");
-		}
+		AlignmentManager.Instance.SetScanButton (scanButtonText);
+
+//		ScanButton.GetComponentInChildren<Text> ().text = scanButtonText;
+//
+//		if (_sessionReady)
+//		{
+//			BackButton.SetActive (true);
+//			ScanButton.SetActive (true);
+//		}
+//		else
+//		{
+//			ScreenMessageController.Instance.SetText ("Initializing Session...");
+//		}
 	}
 
-	public void OnScanClick()
-	{
-		ScreenMessageController.Instance.SetText ("Aligning Camera...");
-		BackButton.SetActive (false);
-
-		MultiframeManager.Instance.OnScanButtonClick ();
-//		AlignmentManager.Instance.Capture ();
-
-//		AlignmentInstrText.SetActive (true);
-//		XRSessionManager.GetSession ().PerformLocalization ();
-	}
+//	public void OnScanClick()
+//	{
+//		ScreenMessageController.Instance.SetText ("Aligning Camera...");
+//		BackButton.SetActive (false);
+//
+//		MultiframeManager.Instance.OnScanButtonClick ();
+////		AlignmentManager.Instance.Capture ();
+//
+////		AlignmentInstrText.SetActive (true);
+////		XRSessionManager.GetSession ().PerformLocalization ();
+//	}
 
 	// Sturfee event called when the Sturfee XR Session is ready to be used
-	private void OnSessionReady()
-	{
-		ScreenMessageController.Instance.ClearText ();
+//	private void OnSessionReady()
+//	{
+//		ScreenMessageController.Instance.ClearText ();
+//
+//		_sessionReady = true;
+//		if (!HasSaveDataPanel.activeSelf)
+//		{
+////			AlignmentManager.Instance.
+//			ScanButton.SetActive (true);
+//
+//			if (_hasSaveData)
+//			{
+//				BackButton.SetActive (true);
+//			}
+//		}
+//	}
 
-		_sessionReady = true;
-		if (!HasSaveDataPanel.activeSelf)
-		{
-//			AlignmentManager.Instance.
-			ScanButton.SetActive (true);
-
-			if (_hasSaveData)
-			{
-				BackButton.SetActive (true);
-			}
-		}
-	}
-
-	// Sturfee event called when camera alignment completes
-	private void OnLocalizationComplete(Sturfee.Unity.XR.Core.Constants.Enums.AlignmentStatus status)
-	{
-		AlignmentInstrText.SetActive (false);
-		if (status == Sturfee.Unity.XR.Core.Constants.Enums.AlignmentStatus.Done)
-		{
-			SturfeeEventManager.Instance.OnSessionReady -= OnSessionReady;
-			SturfeeEventManager.Instance.OnLocalizationComplete -= OnLocalizationComplete;
-
-			ScreenMessageController.Instance.SetText ("Camera Alignment Complete", 3);
-		
-			// TODO: Temporary coroutine until next SDK fixes this issue (Current: v 0.9.1)
-			StartCoroutine(InitializeGame());
-
-			if (_loadGame)
-			{
-				SaveLoadManager.LoadGameData ();
-			}
-		}
-		else if (status == Sturfee.Unity.XR.Core.Constants.Enums.AlignmentStatus.IndoorsError)
-		{
-			ScreenMessageController.Instance.SetText ("Indoors Error. Please try again outside.", 3);
-			ResetToScanPanel ();
-		}
-		else if (status == Sturfee.Unity.XR.Core.Constants.Enums.AlignmentStatus.Error)
-		{
-			ScreenMessageController.Instance.SetText ("Alignment Failed", 3);
-			ResetToScanPanel ();
-		}
-	}
+//	// Sturfee event called when camera alignment completes
+//	private void OnLocalizationComplete(Sturfee.Unity.XR.Core.Constants.Enums.AlignmentStatus status)
+//	{
+//		AlignmentInstrText.SetActive (false);
+//		if (status == Sturfee.Unity.XR.Core.Constants.Enums.AlignmentStatus.Done)
+//		{
+////			SturfeeEventManager.Instance.OnSessionReady -= OnSessionReady;
+//			SturfeeEventManager.Instance.OnLocalizationComplete -= OnLocalizationComplete;
+//
+//			ScreenMessageController.Instance.SetText ("Camera Alignment Complete", 3);
+//		
+//			// TODO: Temporary coroutine until next SDK fixes this issue (Current: v 0.9.1)
+//			StartCoroutine(InitializeGame());
+//
+//			if (_loadGame)
+//			{
+//				SaveLoadManager.LoadGameData ();
+//			}
+//		}
+//		else if (status == Sturfee.Unity.XR.Core.Constants.Enums.AlignmentStatus.IndoorsError)
+//		{
+//			ScreenMessageController.Instance.SetText ("Indoors Error. Please try again outside.", 3);
+//			ResetToScanPanel ();
+//		}
+//		else if (status == Sturfee.Unity.XR.Core.Constants.Enums.AlignmentStatus.Error)
+//		{
+//			ScreenMessageController.Instance.SetText ("Alignment Failed", 3);
+//			ResetToScanPanel ();
+//		}
+//	}
 
 	private void ResetToScanPanel()
 	{
@@ -167,5 +177,29 @@ public class GameManager : MonoBehaviour {
 
 		StreetMap.Instance.InitializeMap();
 		PlayerUi.Initialize ();
+	}
+
+	// TEMP NAME, CHANGE LATER
+	public void InitializeGame2()
+	{
+		if (LoadGame)
+		{
+			SaveLoadManager.LoadGameData ();
+		}
+
+		StreetMap.Instance.InitializeMap();
+		PlayerUi.Initialize ();
+	}
+
+	public void ResetToStartScreen()
+	{
+		if (!_hasSaveData)
+		{
+			ScanButton.SetActive (true);
+		}
+		else
+		{
+			OnStartScreenButtonClick (LoadGame);
+		}
 	}
 }
